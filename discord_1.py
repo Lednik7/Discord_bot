efa86ed4f85e65fa8b5a9b87ed68cd2ef93e657c
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-05-24 19:19:21 +0300

    Update discord_1.py

--- discord_1.py
+++ discord_1.py
@@ -38,70 +38,41 @@ async def on_message(message):
                 
                 await ctx.send(member)
 
-    elif message.content.startswith('!ping_task'):
-        
-        ctx = message.channel
-        
-        message = ((message.content).split())[1:]
-        
+    elif message.content.startswith('!ping'):
+
         try:
-            date = [int(message[i]) for i in range(3)] #день, месяц, год #24 5 2020 @PopularDwarf  0 17 46
+            ctx = message.channel
 
-            hour = int(message[-2]) #17
+            message = ((message.content).replace(".", " ")).replace(":", " ")
 
-            minute = int(message[-1]) #46
+            message = message.split()[1:] #24 5 2020 17 46 1 @PopularDwarf
             
-            message = message[3:] #@PopularDwarf  0 17 46
+            date = [int(message[i]) for i in range(3)] #24 5 2020 17 46 1 @PopularDwarf 
 
-            id = int(message[-3]) #0
-            
+            hour = int(message[3]) #17
+
+            minute = int(message[4]) #46
+
+            id = int(message[5]) #0
+                
+            message = message[6:] #@PopularDwarf 
+
             offset = datetime.timezone(datetime.timedelta(hours=3))
-            
+                
             date = datetime.datetime(date[2], date[1], date[0], hour, minute) - (datetime.datetime.now(offset)).replace(tzinfo=None)
-            
+                
             date = round(date.total_seconds())
 
             print( datetime.datetime.now(offset))
 
             print(date)
-            
+                
             await asyncio.sleep(date)
 
-            await ctx.send("Тебе задание: " + " ".join(message[:-3]) + " "+ str(tasks[id]) + " Время: " + "".join(message[-2]) + ":" + "".join(message[-1]) )
+            await ctx.send("Тебе задание: " + " ".join(message) + ": "+ str(tasks[id]) + "\nВремя: " + str(hour)+ ":" + str(minute) )
             
         except:
+
+            await ctx.send("Что-то пошло не так")
             
-            await ctx.send("Что-то пошло не так. Формат(через пробел): день месяц год id-пользователя номер-таска часы минуты")
-            
-    elif message.content.startswith('!ping'):
-        
-        ctx = message.channel
-        
-        message = ((message.content).split())[1:]
-
-        date = [int(message[i]) for i in range(3)] #день, месяц, год
-
-        hour = int(message[-2])
-
-        minute = int(message[-1])
-        
-        message = message[3:]
-        
-        offset = datetime.timezone(datetime.timedelta(hours=3))
-        
-        date = datetime.datetime(date[2], date[1], date[0], hour, minute) - (datetime.datetime.now(offset)).replace(tzinfo=None)
-        
-        date = round(date.total_seconds())
-
-        print( datetime.datetime.now(offset))
-
-        print(date)
-        
-        await asyncio.sleep(date)
-
-        await ctx.send("Тебе сообщение: " + " ".join(message[:-2]) + " Время: " + "".join(message[-2]) + ":" + "".join(message[-1]) )
-        
-        await ctx.send("ok")
-        
-        
 bot.run(TOKEN)
